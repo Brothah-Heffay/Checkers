580cf5d2fccae949c5b3b234a477fadb7bb10ea5
PensiveBirch <tylerdsnook@gmail.com>
2019-03-20 17:33:03 -0700

    Improved Board.display() to look like a checker board, added space indexes

--- CheckersConsole/Board.cs
+++ CheckersConsole/Board.cs
@@ -48,14 +48,50 @@ namespace CheckersConsole
         //Rudimentary board display method
         public void display()
         {
+            String boardLine = "   ---------------------------------";
+            String botIndex =  "     A   B   C   D   E   F   G   H";
+            int index = 0;
             for (int i = 0; i < 8; i++)
             {
+                switch (i)
+                {
+                    case 0:
+                        index = 8;
+                        break;
+                    case 1:
+                        index = 7;
+                        break;
+                    case 2:
+                        index = 6;
+                        break;
+                    case 3:
+                        index = 5;
+                        break;
+                    case 4:
+                        index = 4;
+                        break;
+                    case 5:
+                        index = 3;
+                        break;
+                    case 6:
+                        index = 2;
+                        break;
+                    case 7:
+                        index = 1;
+                        break;
+                }
+                Console.WriteLine(boardLine);
+                Console.Write(" " + index.ToString());
                 for (int j = 0; j < 8; j++)
                 {
+                    Console.Write(" | ");
                     Console.Write(board[i, j].character);
+                    
                 }
-                Console.Write("\n");
+                Console.Write(" |\n");
             }
+            Console.WriteLine(boardLine);
+            Console.WriteLine(botIndex);
         }
     }
 }
